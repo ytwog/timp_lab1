51a8463cc73db04a2888fb6f8d915c9cb401b24f
ytwog <ytwog@mail.ru>
2020-02-17 14:38:32 +0700

    Added some comments

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_9ca8a.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Program Files/JetBrains/CLion 2019.2.1/bin/cmake/win/share/cmake-3.14/Modules/CMakeCCompilerABI.c" "C:/Abc/docsx/3 course/tech_program/Lab1/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_9ca8a.dir/CMakeCCompilerABI.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
